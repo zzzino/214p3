950c041edad2fc8cf9dd221c51769d5e29222391
Hu, Ricky Binhai <rhu5@rett-imac-14.local>
2018-11-29 11:31:46 -0500

    Initial Commit

---
+++ Ricky_Hu_Project_3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ricky_Hu_Project_3
+//
+//  Created by Hu, Ricky Binhai on 11/29/18.
+//  Copyright © 2018 Hu, Ricky Binhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
