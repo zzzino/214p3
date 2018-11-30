6ccc17995bbeee7eff0adbfb00c3c16cd6c86d51
Hu <rhu5@rett-imac-20.ur.rochester.edu>
2018-11-29 22:08:49 -0500

    wip network

--- Ricky_Hu_Project_3/DriverProfileController.swift
+++ Ricky_Hu_Project_3/DriverProfileController.swift
@@ -11,6 +11,7 @@ import UIKit
 class DriverProfileViewController: UIViewController {
     
     @IBOutlet weak var driverNameLabel: UILabel!
+    @IBOutlet weak var netDescriptionLabel: UILabel!
     
     var selectedDriver : Driver? {
         didSet{
@@ -20,7 +21,41 @@ class DriverProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchDescription()
         driverNameLabel.text = selectedDriver?.name
+    }
+    
+    func fetchDescription() {
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let url = getUrl(name: (selectedDriver?.name)!)
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request){
+            (data, response, error) -> Void in
+            
+            if let jsonData = data {
+                if let jsonString = String(data: jsonData, encoding: .utf8) {
+                    return jsonString
+                }
+            } else if let requestError = error {
+                print("Error \(requestError)")
+            } else {
+                print("Error case 2")
+            }
+        }
+        task.resume()
+    }
+    
+    func getUrl(name: String) -> URL {
+        let baseUrl = "https://csc214-p3-server.herokuapp.com"
+        var components = URLComponents(string: baseUrl)!
+        var queryItems = [URLQueryItem]()
+        
+        let driverNameParam = URLQueryItem(name: "name", value: name)
+        queryItems.append(driverNameParam)
+        
+        
+        components.queryItems = queryItems
+        return components.url!
     }
     
 }
